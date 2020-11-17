4d1b9036e95c310f6e3b7a6d5d6d651eaaafd752
phpilatso <phenyopilatso@momentum.co.za>
2020-11-17 14:56:35 +0200

    adding beer-service local profile for MySQL

---
+++ src/test/java/guru/sfg/brewery/ConfigRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.sfg.brewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigRepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
